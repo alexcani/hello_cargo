40bfa3ba6256f340adaa1ef5cce4bd4121bed1ca
Alex Cani <alex.consulting.ag@hexagon.com>
2022-07-05 18:18:05 -0300

    Crate rules

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
+    // Package = top level, cargo feature that contains one or more crates
+    // and their relationships. Has a Cargo.toml file
+    // Crate = binary or library (essentially cmake targets?)
+    // Packages can have as many binary crates as wanted but only 1 library crate
+
+    // src/main.rs -> crate root of binary crate of same name as package
+    // src/lib.rs -> crate root of library crate of same name as package
+    // Can have both at the same time
+
+    // For multiple binary crates, place files in src/bin directory
     println!("Hello, world!");
 }
