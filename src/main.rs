16180602fda6c7fadb695753e6bded9fbe23f75e
Alex Cani <alex.consulting.ag@hexagon.com>
2022-07-08 15:47:34 -0300

    Strings and utf-8

--- src/main.rs
+++ src/main.rs
@@ -34,4 +34,48 @@ fn main() {
     }  // v goes out of scope and is freed
 
 
+
+    // Strings
+    let mut s = String::new();  // new string
+    s.push('c'); // for chars
+    s.push_str("for slices");
+
+    let r = "string_literal";
+    let t = r.to_string();  // from literal to String
+    let v = "string_literal".to_string();  // oneliner
+    let n = String::from("yet another way");
+
+    println!("{} {} {} {}", s, t, v, n);
+
+    // Strings (and str) are UTF-8
+    let x = String::from("😋");
+    println!("{}", x);
+
+    println!("concatenate: {}", n + &x);  // + works on string slices (&str) not String
+                                          // also 'n' has been moved
+    // println!("{}", n); won't work
+
+    let tic = "tic".to_string();
+    let tac = "tac".to_string();
+    let toe = "toe".to_string();
+    let s = tic + "-" + &tac + "-" + &toe;  // only tic is moved
+    println!("{}", s);
+    // println!("{}", tic);  won't work
+    // Using format
+    let tic = "tic".to_string();
+    let s = format!("{}-{}-{}", tic, tac, toe);  // no one is moved
+    println!("{} {}", s, tic);  // works
+
+    // Indexing strings
+    // Can't index properly like in most languages because UTF-8 encoding
+    // has variable length. Even string slices are dangerous, because the boundaries
+    // must match the characters boundaries
+    for c in "नमस्ते".chars() {  // .chars iterates over each valid UTF-8 character
+        // which is not necessarely a grepheme cluster (a "full" character)
+        println!("{}", c);
+    }
+
+    for b in "नमस्ते".bytes() {  // .bytes() returns each byte, 18 in this case
+        println!("{}", b);
+    }
 }
