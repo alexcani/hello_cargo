8a97041ea1d553cd7252cc8d997f455152d5a8ae
Alex Cani <alex.consulting.ag@hexagon.com>
2022-07-12 17:26:31 -0300

    Traits and implementations

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,29 @@ fn main() {
 
     // dbg!(integer_point.distance_from_origin());  // not defined for integers
     dbg!(float_point.distance_from_origin());
+
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from(
+            "The Pittsburgh Penguins once again are the best \
+             hockey team in the NHL.",
+        ),
+    };
+
+    println!("New article available! {}", article.summarize());
 }
 
 fn largest<T>(list: &[T]) {//-> T {
@@ -50,3 +73,51 @@ impl Point<f32> {  // impl is not generic, specific for f32
         (self.x.powi(2) + self.y.powi(2)).sqrt()
     }
 }
+
+
+// Traits
+pub trait Summary {
+    fn summarize(&self) -> String {
+        // Default implementation
+        format!("(Read more...)")
+    }
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {}  // use default implementation
+
+/*impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}*/
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+// Can implement trait for type anywhere within the crate (as long as things are in scope)
+mod teste1 {
+    mod teste2 {
+use super::super::{Tweet, Summary};
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
+    }
+}
+
+// the only restriction is called coherence: either the type or the trait (or both) must be from
+// the local crate
