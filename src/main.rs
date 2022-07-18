ecee5f8a832726d66a59d131f44ce4d84b9d7497
Alex Cani <alex.consulting.ag@hexagon.com>
2022-07-18 16:25:56 -0300

    Take command line arguments. Create file with words

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let arguments: Vec<String> = env::args().collect();
+
+    println!("Arguments: {:#?}", arguments);
+
+    let needle = &arguments.get(1).expect("Please provide 2 arguments");
+    let haystack = &arguments.get(2).expect("Please provide 2 arguments");
+
+    println!("Search for {} in {}", needle, haystack);
 }
