29efb724b0fddbfac219c9d686ea62a5e614413d
Alex Cani <alexcani109@gmail.com>
2022-07-21 01:06:30 +0100

    Improve config parsing

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,28 @@ use std::fs;
 fn main() {
     let arguments: Vec<String> = env::args().collect();
 
+    let config = Config::new(&arguments);
+
     println!("Arguments: {:#?}", arguments);
+    println!("Search for {} in {}", config.query, config.filename);
 
-    let needle = &arguments.get(1).expect("Please provide 2 arguments");
-    let haystack = &arguments.get(2).expect("Please provide 2 arguments");
-
-    println!("Search for {} in {}", needle, haystack);
-
-    let file_content = fs::read_to_string(haystack).expect("Something went wrong with reading the file");
+    let file_content = fs::read_to_string(config.filename).expect("Something went wrong with reading the file");
     println!("File contents: {}", file_content);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let needle = args.get(1).expect("Please provide 2 arguments").clone();
+        let haystack = args.get(2).expect("Please provide 2 arguments").clone();
+
+        Config {
+            query: needle,
+            filename:haystack
+        }
+    }
 }
