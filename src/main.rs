96ff40258c03f8dadf38d89ba0bdda472bb3a250
Alex Cani <alex.consulting.ag@hexagon.com>
2022-08-11 23:57:38 +0200

    Deref coersion and Deref trait

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::ops::Deref;
+
 fn main() {
     // Smart pointers
     // Box simply points to heap memory, no overhead
@@ -15,8 +17,10 @@ fn main() {
     // For when you can't know the size of a type at compile time
     use List::{Cons, Nil};
     let con_list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Cons(4, Box::new(Nil))))))));
-    dbg!(&con_list);
     println!("{:?}", con_list);
+
+    deref_operator_tests();
+    drop_trait_tests();
 }
 
 // Cons list example using Box
@@ -25,4 +29,54 @@ fn main() {
 enum List {
     Cons(i32, Box<List>),  // contains the value and the next element. List needs to be a Box so it has fixed size
     Nil               // or contains Nil
+}
+
+fn deref_operator_tests() {
+    let x = 5;
+    let y = &x;
+    assert_eq!(5, x);
+    // assert_eq!(5, y); fails
+    assert_eq!(5, *y);
+
+    println!("{} {} {}", x, y, *y);  // coersion happens here
+
+    // Using Box as reference
+    let z = Box::new(x);
+    assert_eq!(5, *z);
+
+    // Using custom smart pointer
+    let x = 5;
+    let y = MyBox::new(x);
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    // Deref coercion to &str
+    let x = MyBox::new(String::from("hey"));
+    hello(&x);  // &MyBox gets coersed into &String then &str because it implements Deref trait
+}
+
+// Our custom smart pointer
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {  // deref trait
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+fn hello(x: &str) {
+    println!("Hello, {x}");
+}
+
+
+// Drop trait
+fn drop_trait_tests() {
+
 }
