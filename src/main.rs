f205d9b13e7152d817b5ab1d6b1330f368e1e3b8
Alex Cani <alex.consulting.ag@hexagon.com>
2022-08-19 00:39:53 +0200

    Weak ptrs, reference cycles

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::cell::RefCell;
+use std::borrow::Borrow;
+use std::cell::{RefCell, Ref};
 use std::ops::Deref;
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
+use std::vec;
 
 fn main() {
     // Smart pointers
@@ -25,6 +27,7 @@ fn main() {
     drop_trait_tests();
     rc_pointer_tests();
     refcell_tests();
+    weak_ptr_tests();
 }
 
 // Cons list example using Box
@@ -156,4 +159,52 @@ fn refcell_tests() {
     }
 
     println!("{:?}\n{:?}\n{:?}", a, b, c);
+}
+
+#[derive(Debug)]
+struct Node {
+    value: i32,
+    children: RefCell<Vec<Rc<Node>>>,
+    parent: RefCell<Weak<Node>>,
+}
+
+fn weak_ptr_tests() {
+    let leaf = Rc::new(Node {
+        value: 3,
+        children: RefCell::new(vec![]),
+        parent: RefCell::new(Weak::new()),
+    });
+
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf),
+    );
+{
+    let branch = Rc::new(Node {
+        value: 5,
+        children: RefCell::new(vec![Rc::clone(&leaf)]),
+        parent: RefCell::new(Weak::new()),
+    });
+
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+    println!(
+        "branch strong = {}, weak = {}",
+        Rc::strong_count(&branch),
+        Rc::weak_count(&branch),
+    );
+
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf),
+    );
+}
+    println!("leaf parent: {:?}", leaf.parent.borrow().upgrade());
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf),
+    );
 }
